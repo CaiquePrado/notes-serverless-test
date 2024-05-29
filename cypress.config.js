d49428b60a5b99a26986eaa7f4baaa45e58fcf7c
CaiquePrado <caiqueprado76@gmail.com>
2024-05-29 15:55:08 -0300

    chore: config baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require("cypress")
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+      return {
+        baseUrl: "https://notes-serverless-app.com"
+      }
+    }
+  }
+})
